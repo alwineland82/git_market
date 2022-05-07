26a1fbbaac480870b09d75acec48fa2f08a7e36b
al_wineland <alwineland@yandex.ru>
2022-05-07 20:55:43 +1100

    database change

--- market.py
+++ market.py
@@ -11,6 +11,8 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    available = db.Column(db.Integer, nullable=False)
+    
 
     def __repr__(self):
         return f'Item {self.name}'
